8825bbfcf5fa9ac48ffa4759597773f28acfe486
Toto <tudor.i.farcas1@gmail.com>
2023-12-04 16:05:40 +0200

    demo

--- app/src/main/java/com/example/project_is/activity/RegisterActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.project_is.activity
-
-class RegisterActivity {
-}
--- app/src/main/java/com/example/project_is/ui/lockscreen/lockscreen_register/RegisterFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.example.project_is.ui.lockscreen.lockscreen_register
-
-class RegisterFragment {
-}
