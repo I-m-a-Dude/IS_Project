48e70860477ab1b10f91c7e0f83f414647442a78
Toto <tudor.i.farcas1@gmail.com>
2023-12-25 12:41:12 +0200

    12th time

--- app/src/main/java/com/toto/proiect_is/screen/HomeActivity.java
+++ app/src/main/java/com/toto/proiect_is/screen/HomeActivity.java
@@ -6,14 +6,9 @@ import android.view.View;
 import android.widget.Button;
 
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import com.toto.proiect_is.R;
-import com.toto.proiect_is.screen.LogInActivity;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -21,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button logoutButton = findViewById(R.id.logoutButton);
+        Button logoutButton = findViewById(R.id.logout);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
